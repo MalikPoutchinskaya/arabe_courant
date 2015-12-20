7816227bef62a9dbac30b227a3de3a78d8a94568
Tortank <malikbouras.cesi@gmail.com>
2015-12-20 23:37:10 +0100

    slide fragement

---
+++ app/src/main/java/com/poutchinskaya/malik/sotunisia/Presentation/Choix_langues/ChoixLangue_activity.java
@@ -0,0 +1,22 @@
+package com.poutchinskaya.malik.sotunisia.Presentation.Choix_langues;
+
+/**
+ * Created by Malik on 24/08/2015.
+ */
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+import com.poutchinskaya.malik.sotunisia.R;
+
+public class ChoixLangue_activity extends FragmentActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choix_langues);
+
+    }
+
+}
